6afd3735bb658e306ece493e6123d4766fcbcb6b
anniepeak <147626089+anniepeak@users.noreply.github.com>
2024-10-24 01:08:20 -0400

    Update templete.js

--- templete.js
+++ templete.js
@@ -2,7 +2,7 @@
 const iris = d3.csv("iris.csv");
 
 // Once the data is loaded, proceed with plotting
-penguins.then(function(data) {
+iris.then(function(data) {
     // Convert string values to numbers
     data.forEach(function(d) {
         d.PetalLength = +d.PetalLength;
@@ -10,29 +10,64 @@ penguins.then(function(data) {
     });
 
     // Define the dimensions and margins for the SVG
+    const margin = {top: 50, right: 50, bottom: 50, left: 50};
+    const width = 500 - margin.left - margin.right;
+    const height = 400 - margin.top - margin.bottom;
 
 
     // Create the SVG container
-    
+    const svg = d3.select("body").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
     
     // Set up scales for x and y axes
     // d3.min(data, d => d.bill_length_mm)-5
+    const xScale = d3.scaleLinear()
+        .domain([d3.min(data, d => d.PetalLength) - 0.5, d3.max(data, d => d.PetalLength) + 0.5])
+        .range([0, width]);
+
+    const yScale = d3.scaleLinear()
+        .domain([d3.min(data, d => d.PetalWidth) - 0.5, d3.max(data, d => d.PetalWidth) + 0.5])
+        .range([height, 0]);
 
     const colorScale = d3.scaleOrdinal()
         .domain(data.map(d => d.Species))
         .range(d3.schemeCategory10);
 
     // Add scales     
-    
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(xScale));
+
+    svg.append("g")
+        .call(d3.axisLeft(yScale));
 
     // Add circles for each data point
-    
+    svg.selectAll("circle")
+        .data(data)
+        .enter().append("circle")
+        .attr("cx", d => xScale(d.PetalLength))
+        .attr("cy", d => yScale(d.PetalWidth))
+        .attr("r", 5)
+        .style("fill", d => colorScale(d.Species));
+
 
     // Add x-axis label
-    
+    svg.append("text")
+        .attr("x", width / 2)
+        .attr("y", height + margin.bottom - 10)
+        .attr("text-anchor", "middle")
+        .text("Petal Length");
 
     // Add y-axis label
-    
+    svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("x", -height / 2)
+        .attr("y", -margin.left + 10)
+        .attr("text-anchor", "middle")
+        .text("Petal Width");
 
     // Add legend
     const legend = svg.selectAll(".legend")
@@ -41,9 +76,22 @@ penguins.then(function(data) {
         .attr("class", "legend")
         .attr("transform", (d, i) => "translate(0," + i * 20 + ")");
 
+    legend.append("rect")
+        .attr("x", width - 18)
+        .attr("y", 9)
+        .attr("width", 18)
+        .attr("height", 18)
+        .style("fill", colorScale);
+
+    legend.append("text")
+        .attr("x", width - 24)
+        .attr("y", 18)
+        .attr("dy", ".35em")
+        .style("text-anchor", "end")
+        .text(d => d);
 });
 
-penguins.then(function(data) {
+iris.then(function(data) {
     // Convert string values to numbers
     
 
